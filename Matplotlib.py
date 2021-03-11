6ecdf958369bb194b2d3fc6a47b25abc32f30c64
chisoftltd <chisoftltd@gmail.com>
2021-03-11 13:59:32 +0100

    Matplotlib Markers

--- Matplotlib.py
+++ Matplotlib.py
@@ -18,13 +18,43 @@ plt.plot(xpoints, ypoints, 'o')
 
 # Multiple Points
 xpoints = np.array([6, 66, 34, 260])
-ypoints = np.array([6, 260, 6, 166])
-plt.plot(xpoints, ypoints)
-plt.show()
+# ypoints = np.array([6, 260, 6, 166])
+# plt.plot(xpoints, ypoints)
+# plt.show()
 
 # Default X-Points
-plt.plot(ypoints)
+# plt.plot(ypoints)
+# plt.show()
+
+# plt.plot(xpoints)
+# plt.show()
+
+
+# Matplotlib Markers
+# Markers
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, marker = 'o')
+# plt.show()
+
+# plt.plot(xpoints, marker = 'o')
+# plt.show()
+
+# plt.plot(ypoints, marker = '*')
+# plt.show()
+
+# plt.plot(xpoints, marker = '*')
+# plt.show()
+
+# Format Strings fmt
+plt.plot(ypoints, 'o:r')
 plt.show()
 
-plt.plot(xpoints)
+# Line Reference
+plt.plot(ypoints, 'o-:')
+plt.show()
+
+plt.plot(ypoints, 'o--')
+plt.show()
+
+plt.plot(ypoints, 'o-.')
 plt.show()
